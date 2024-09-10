d59ebb65f444101960dc212a09172380e06efb36
paulpham157 <paulpham157@gmail.com>
2024-09-10 22:28:06 +0700

    .

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ REM Install requirements
 pip install -r requirements.txt
 
 REM Build the executable
-pyinstaller --name="Download playlist YT as audio files" --windowed --onefile app.py
+pyinstaller --name="YoutubeDownloaderByPaulPham157" --windowed --onefile app.py
 
 echo Build completed. The executable is in the 'dist' folder.
