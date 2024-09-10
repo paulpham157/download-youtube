2a5ee7971a21cda63788b4fecbe67aa59b6991ef
paul-pham-157 <phamdinhhung2302@gmail.com>
2024-09-10 22:28:06 +0700

    .

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 pip install -r requirements.txt
 
 # Build the executable
-pyinstaller --name="Download playlist YT as audio files" --windowed --onefile app.py
+pyinstaller --name="YoutubeDownloaderByPaulPham157" --windowed --onefile app.py
 
 echo "Build completed. The executable is in the 'dist' folder."
